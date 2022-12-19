f083067769c807892c1d2c9d6962239d2996bcf0
Snokxyo <m.el.2000@mail.ru>
2022-12-19 18:52:29 +0500

    2 version

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 
 // подключение к БД
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=gloevk2022", 'root', '');
+    $conn = new PDO("mysql:host=localhost;dbname=base", 'root', '');
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
